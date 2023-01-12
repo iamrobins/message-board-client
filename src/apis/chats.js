4d946127c032f36cb11702dec1e08e4516fd0210
iamrobins <itsrobin4@gmail.com>
2023-01-12 18:48:28 +0530

    updated endpoint with HOST from envs

--- src/apis/chats.js
+++ src/apis/chats.js
@@ -1,5 +1,7 @@
+const HOST = process.env.REACT_APP_HOST;
+
 export async function postMessage(message) {
-    const response = await fetch("http://localhost:8000/api/v1/message/", {
+    const response = await fetch(`${HOST}api/v1/message/`, {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ content: message })
@@ -11,10 +13,20 @@ export async function postMessage(message) {
 }
 
 export async function getMessages() {
-    const response = await fetch("http://localhost:8000/api/v1/message/", {
+    const response = await fetch(`${HOST}api/v1/message/`, {
         method: "GET",
         headers: { "Content-Type": "application/json" }
     })
     const data = await response.json();
     return data;
+}
+
+export async function deleteMessage(secret, id=null) {
+    const response = await fetch(`${HOST}api/v1/message/?id=${id}`, {
+        method: "DELETE",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ secret })
+    })
+    if (response.status !== 204)
+        throw Error("Unable to process delete request");
 }
