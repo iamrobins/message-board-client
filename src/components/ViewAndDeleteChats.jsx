a6bad9b0f45145b09d205fcda86bd572e9fd11b4
iamrobins <itsrobin4@gmail.com>
2023-01-12 18:49:22 +0530

    delete selected message feature added

--- src/components/ViewAndDeleteChats.jsx
+++ src/components/ViewAndDeleteChats.jsx
@@ -1,33 +1,42 @@
-import { HStack, Text, VStack, Link, useColorModeValue} from "@chakra-ui/react"
+import { HStack, Text, VStack, Link, useColorModeValue, useDisclosure } from "@chakra-ui/react"
 import { FaFacebookMessenger } from "react-icons/fa"
-import {useQuery} from "react-query";
-import {chats} from "../apis";
- 
-function Message({content, created_at}) {
+import { useQuery } from "react-query";
+import { chats } from "../apis";
+import DeleteWithToken from "./DeleteWithToken";
+
+function Message({ content, createdAt, id }) {
+    const { isOpen, onToggle, onClose } = useDisclosure();
+
     return (
         <VStack border={"dotted"} borderWidth="medium" borderColor="gray.200" pl="4" pr="8" py="2">
             <HStack>
                 <FaFacebookMessenger />
                 <Text fontWeight={"bold"}>~anonymous - </Text>
-                <Text fontWeight={"light"} fontSize="xs">{new Date(created_at).toLocaleTimeString()}</Text>
-                <Link fontSize={"sm"} color="blue.600">Delete</Link>
+                <Text fontWeight={"light"} fontSize="xs">{new Date(createdAt).toLocaleTimeString()}</Text>
+                <Link fontSize={"sm"} color="blue.600" onClick={onToggle}>Delete</Link>
             </HStack>
             <Text color={useColorModeValue("gray.500", "gray.200")} w="100%">{content}</Text>
+
+            {/* Popup for Deletion */}
+            <DeleteWithToken isOpen={isOpen} onClose={onClose} onToggle={onToggle} id={id} />
         </VStack>
     )
 }
 
 export default function ViewAndDeleteChats() {
-    const {isLoading, error, data} = useQuery("messages", chats.getMessages, {
+    const { isLoading, error, data } = useQuery("messages", chats.getMessages, {
         refetchInterval: 1000
     });
 
-    if(isLoading || error || !data)
+    if (isLoading || error || !data)
         return <></>
 
     return (
         <VStack w="100%" alignItems="flex-start" spacing={"8"} h="400px" overflowY={"auto"}>
-            {data.map(message => <span key={message.id}><Message content={message.content} created_at={message.created_at} /></span>)}
+            {data.map(message => 
+                <span key={message.id}>
+                    <Message content={message.content} createdAt={message.created_at} id={message.id} />
+                </span>)}
         </VStack>
     )
 }
